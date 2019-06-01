a90f44d806c3eec8de0439517c86f154467d2163
S Yoon <sohizzhang@gmail.com>
2019-06-01 21:41:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,11 @@
+def valid_move?(board, index)
+  value = board[index] - 1
+  if value.inbetween?(0, 8)
+    true
+  else
+    false 
+end 
+
 def position_taken?(board, index)
   value = board[index]
   if value == " " || value == "" || value == nil
